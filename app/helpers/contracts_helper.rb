71e500c90a04cf276ac5faab54bbc1f6572c7c68
adrenalinealie <alizee.albrecht17@gmail.com>
2018-04-21 06:48:58 +0000

    Changes to contracts helper

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,2 +1,51 @@
 module ContractsHelper
+    require 'docusign_api'
+    require 'curl'
+    require 'json'
+    require 'base64'
+    
+    #new instance of docusign API
+  @api = DocusignApi.new \
+  username: 'alizee.albrecht17@gmail.com', \
+  password: '58317Alizee', \
+  integrator_key: 'e5fdb0cd-a206-4934-91f1-5d80834a4989', \
+  login_url: 'https://demo.docusign.net/restapi/v2/login_information'
+  
+  #Create enveloppe from template'
+    def self.create_enveloppe
+      
+        template_id = '16ddd44d-ebef-4dba-9245-388a8de6686d'
+        h = {
+    emailSubject: 'Listing Signature',
+    status: 'created',
+    templateRoles: [],
+    compositeTemplates: [{
+      serverTemplates: [
+        {
+          sequence: '1',
+          templateId: template_id
+        }
+      ],
+      document: {
+        name: 'Listing Agreement',
+        documentId: '01',
+        #documentBase64: Base64.encode64(File.read('Agreement')),
+        documentFields: [
+          { name: 'SellerName', value: PodioHelper.get_field(name,"client") },
+          #{ name: 'field2', value: 'value2' }
+        ]
+      }
+    }]
+  }
+  
+  c = @api.post '/envelopes', h.to_json
+
+  puts c.response_code
+  puts JSON::parse(c.body)
+    end
 end
+ 
+
+ 
+ 
+
--- test/controllers/contracts_controller_test.rb
+++ test/controllers/contracts_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
-
 class ContractsControllerTest < ActionController::TestCase
   test "should get listing-agreement" do
     get :listing-agreement
     assert_response :success
-  end
+  end 
 
 end
